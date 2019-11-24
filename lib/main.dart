10a34af542c3a25cec2cd6110f43f17d648b7a67
Hamed <hamed@firstsource.io>
2019-11-24 16:49:08 +0330

    - add line chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'package:charts_flutter_demo/simple.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Charts Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Center(),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Charts Flutter Demo'),),
+        body: SimpleLineChart(),
+      ),
     );
   }
 }
